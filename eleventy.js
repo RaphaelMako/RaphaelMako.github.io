44c9ba3cf5106cf0a3166f1a912106c2c699ba2b
Raphael Mako <makoto.sorensen@gmail.com>
2026-04-29 15:33:58 -0400

    Trying to get Eleventy to work :(

--- eleventy.js
+++ eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function (eleventyConfig) {
     // Copy 'public' folder to '_site/public'
-    eleventyConfig.addPassthroughCopy("public");
-
+    // eleventyConfig.addPassthroughCopy("./public/");
     return {
         dir: {
-            input: ".",
-            output: "_site" // This is what GitHub Pages will serve
+            input: "blog_src",
+            includes: "_includes",
+            output: "blog" // This is what GitHub Pages will serve
         }
     };
 };
